ec6bb179c1d3a71aba792b62aff4fe127af9d5f0
basimsherif <basim@insidestorybox.com>
2013-09-20 18:34:48 +0530

    chnage

--- Demo_Git2/src/com/example/demo_git2/MainActivity.java
+++ Demo_Git2/src/com/example/demo_git2/MainActivity.java
@@ -17,8 +17,9 @@ public class MainActivity extends Activity {
 		// Inflate the menu; this adds items to the action bar if it is present.
 		getMenuInflater().inflate(R.menu.main, menu);
 		return true;
+		
+		//Change by basim
+		
 	}
 
-	
-	//THIS IS RIJO CHARLES
 }
